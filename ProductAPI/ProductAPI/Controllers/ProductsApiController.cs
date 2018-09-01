8a225e34d32a2ca012323f9522c0a3768aea79b1
Mozzey <mbcacciano@gmail.com>
2018-09-01 12:08:50 -0400

    calling products endpoint only returns prodID and prodName

---
+++ ProductAPI/ProductAPI/Controllers/ProductsApiController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ProductAPI;
+using ProductAPI.Models;
+
+namespace ProductAPI.Controllers
+{
+    public class ProductsApiController : ApiController
+    {
+        private ProductAPIDB db = new ProductAPIDB();
+
+        // GET: api/ProductsApi
+        public IQueryable<ProductViewModel> GetProducts()
+        {
+            var model = db.Products.Select(x => new ProductViewModel
+            {
+                ProductId = x.ProductID,
+                ProductName = x.ProductName
+            });
+            return model.OrderBy(x => x.ProductId);
+        }
+
+        //[ResponseType(typeof(CategoryViewModel))]
+        //public IHttpActionResult GetCategories(int catId)
+        //{
+        //    var category = db.Products.Select(x => new CategoryViewModel
+        //    {
+        //        CategoryId = x.CategoryID
+        //    });
+        //    Product product = db.Products.Find(category);
+        //    if (product == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Ok(category);
+        //}
+
+        // GET: api/ProductsApi/5
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult GetProduct(int id)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        // PUT: api/ProductsApi/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProduct(int id, Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != product.ProductID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ProductsApi
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult PostProduct(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Products.Add(product);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = product.ProductID }, product);
+        }
+
+        // DELETE: api/ProductsApi/5
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult DeleteProduct(int id)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            db.Products.Remove(product);
+            db.SaveChanges();
+
+            return Ok(product);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProductExists(int id)
+        {
+            return db.Products.Count(e => e.ProductID == id) > 0;
+        }
+    }
+}
